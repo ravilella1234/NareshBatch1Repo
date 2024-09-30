9452b836052960983e5e2edbb5970a4b81bbff19
ravilella1234 <masters.selenium@gmail.com>
2024-09-30 12:23:59 +0530

    File2

---
+++ FirstProject/src/kotak/loans/homeloans/File1.java
@@ -0,0 +1,22 @@
+package kotak.loans.homeloans;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+public class File1 
+{
+
+	public static void main(String[] args) throws Exception 
+	{
+		File f = new File("C:\\Users\\DELL\\Desktop\\text1.txt");
+		
+		FileInputStream fis = new FileInputStream(f);
+		//FileInputStream fis1 = new FileInputStream("C:\\Users\\DELL\\Desktop\\text1.txt");
+		int asciiNum = 0;
+		while((asciiNum = fis.read()) != -1)
+			System.out.print((char)asciiNum);
+		
+		fis.close();
+	}
+
+}
