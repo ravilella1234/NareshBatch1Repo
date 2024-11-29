3afb24abc79f8018a221d56644c3e724866cc84c
ravilella1234 <masters.selenium@gmail.com>
2024-11-29 12:47:06 +0530

    Cucumber Day-3

--- MavanCucumber/src/test/java/testcases/Login.java
+++ MavanCucumber/src/test/java/testcases/Login.java
@@ -7,47 +7,48 @@ public class Login
 	
 	@Given("The user navigates to the login page")
 	public void the_user_navigates_to_the_login_page() {
-	    System.out.println("---> user has navigated to login page...");
+	    System.out.println("--> user has navigated to the login page");
+	}
+
+	@When("The user enters valid email {string}")
+	public void the_user_enters_valid_email(String userName) {
+	    System.out.println("---> user has entered valid email :" + userName);
 	}
 
 	@And("Enter valid password {string}")
-	public void enter_valid_password_ravi123456(String userpassword) {
-		System.out.println("---> user has entered valid password : " + userpassword);
+	public void enter_valid_password(String userPassword) {
+	    System.out.println("---> User has Entered valid password :" + userPassword);
 	}
 
 	@And("Clicks the login button")
 	public void clicks_the_login_button() {
-		System.out.println("---> user has clicked the login button");
+	   System.out.println("---> User has Clicked the login button");
 	}
 
 	@Then("The user should be redirected to the dashboard")
 	public void the_user_should_be_redirected_to_the_dashboard() {
-		System.out.println("---> Logedin user has redirected to the dashboard");
+	   System.out.println("---> User has redirected to the dashboard");
 	}
 
-	@When("The user enters invalid email  {string}")
-	public void the_user_enters_invalid_email_ravilella_gmail_com(String userName) {
-		System.out.println("---> user has entered invalid email credentials : " + userName);
+	@When("The user enters invalid email {string}")
+	public void the_user_enters_invalid_email(String userName) {
+	    System.out.println("---> user has entered invalid email : " + userName);
 	}
 
 	@And("Enter invalid password {string}")
-	public void enter_invalid_password_ravi123456(String userpassword) {
-		System.out.println("---> user has entered invalid password : " + userpassword);
+	public void enter_invalid_password(String userPassword) {
+		System.out.println("---> user has entered invalid password : " + userPassword);
 	}
 
 	@Then("An error message Invalid username or password should be displayed")
 	public void an_error_message_invalid_username_or_password_should_be_displayed() {
-		System.out.println("---> error message for Invalid username or password is displaying");
+	   System.out.println("---> error message has displayed for Invalid username or password ");
 	}
 
 	@Then("user should get proper warning message")
 	public void user_should_get_proper_warning_message() {
-		System.out.println("---> user is getting proper warning message");
+	    System.out.println("---> user has got proper warning message");
 	}
-
-	@When("The user enters valid email  {string}")
-	public void the_user_enters_valid_email_ravilella_gmail_com(String userName) {
-		System.out.println("---> user has entered valid email  : " + userName);
-	}
-
+		
+	
 }
--- MavanCucumber/target/test-classes/testcases/login.feature
+++ MavanCucumber/target/test-classes/testcases/login.feature
@@ -1,33 +1,37 @@
 Feature: User Login  
   As a user, I want to log in to the application so that I can access my account.  
 
-  Scenario: Successful login with valid credentials  
+  Scenario Outline:  Successful login with valid credentials  
     Given The user navigates to the login page  
-    When The user enters valid email “ravilella@gmail.com”
-    And Enter valid password “ravi123456”
+    When The user enters valid email "<username>"
+    And Enter valid password "<password>"
     And Clicks the login button  
     Then The user should be redirected to the dashboard  
     
+    Examples:
+    | username  | password  |
+    | user1     | pass123   | 
+    | user2     | pass456   |
+    | user3     | pass789   | 
+    
+    
   Scenario: Login attempt with invalid credentials  
     Given The user navigates to the login page  
-    When The user enters invalid email  “ravilella@gmail.com”
-    And Enter invalid password “ravi123456”
+    When The user enters invalid email "ravilella@gmail.com"
+    And Enter invalid password "ravi123456"
     And Clicks the login button  
     Then An error message Invalid username or password should be displayed  
 
   Scenario: Login with invalid user and valid password credentials  
     Given The user navigates to the login page  
-    When The user enters invalid email  “ravilella@gmail.com”
-    And Enter valid password “ravi123456”
+    When The user enters invalid email "ravilella@gmail.com"
+    And Enter valid password "ravi123456"
     And Clicks the login button  
     Then user should get proper warning message
 
   Scenario: Login with valid user and invalid password credentials  
     Given The user navigates to the login page
-    When The user enters valid email  “ravilella@gmail.com”
-    And Enter invalid password “ravi123456”
+    When The user enters valid email "ravilella@gmail.com"
+    And Enter invalid password "ravi123456"
     And Clicks the login button  
     Then user should get proper warning message
-   
-  	
-	
