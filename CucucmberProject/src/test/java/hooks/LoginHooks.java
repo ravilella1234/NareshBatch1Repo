f1414dfeeab0f82b2d88b75cf4cd313aaa125948
ravilella1234 <masters.selenium@gmail.com>
2024-12-04 12:48:44 +0530

    project added

---
+++ CucucmberProject/src/test/java/hooks/LoginHooks.java
@@ -0,0 +1,19 @@
+package hooks;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class LoginHooks 
+{
+	@Before
+	public void setup()
+	{
+		
+	}
+	
+	@After
+	public void killProcess()
+	{
+		
+	}
+}
---
+++ CucucmberProject/src/test/java/steps/LoginDemo.java
@@ -0,0 +1,60 @@
+package steps;
+
+import io.cucumber.java.en.*;
+
+public class LoginDemo 
+{
+	@Given("the user is on the login page")
+	public void the_user_is_on_the_login_page() 
+	{
+	   
+	}
+
+	@When("the user enters {string} into the username field")
+	public void the_user_enters_into_the_username_field(String string) 
+	{
+	   
+	}
+
+	@When("the user enters {string} into the password field")
+	public void the_user_enters_into_the_password_field(String string) 
+	{
+	   
+	}
+
+	@When("the user clicks the Submit button")
+	public void the_user_clicks_the_submit_button() 
+	{
+	    
+	}
+
+	@Then("the new page URL should contain {string}")
+	public void the_new_page_url_should_contain(String string) 
+	{
+	  
+	}
+
+	@Then("the new page should display text containing {string}")
+	public void the_new_page_should_display_text_containing(String string) 
+	{
+	    
+	}
+
+	@Then("the {string} button should be visible on the new page")
+	public void the_button_should_be_visible_on_the_new_page(String string) 
+	{
+	   
+	}
+
+	@Then("an error message should be displayed")
+	public void an_error_message_should_be_displayed() 
+	{
+	   
+	}
+
+	@Then("the error message text should be {string}")
+	public void the_error_message_text_should_be(String string) 
+	{
+	   
+	}
+}
